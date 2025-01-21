028ebafcf3858cb63796de919791d77050478561
afmachin <afmachin@gmail.com>
2025-01-20 19:38:39 -0500

    CSCI-361-2025: Initial Commit

---
+++ src/assignments/sorting_algorithms/merge_sort/MergeSort.java
@@ -0,0 +1,13 @@
+package assignments.sorting_algorithms.merge_sort;
+
+import java.util.List;
+
+public class MergeSort {
+    public static List<Person> sort(List<Person> people) {
+
+    }
+
+    private static List<Person> merge(List<Person> left, List<Person> right) {
+
+    }
+}
